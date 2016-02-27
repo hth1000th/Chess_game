35d9ff6989e4fbafb4fa90226bf11544d4f53b1a
Andy Hwang <tahwang@puc.edu>
2016-02-26 18:02:42 -0800

    modified version for test on CLI

--- test_chess.rb
+++ test_chess.rb
@@ -19,10 +19,11 @@ class Chess_Test < Test::Unit::TestCase
     assert_nil(game.setup, "This is not nil.")
     assert_match(/king|queen|bishop|knight|rook|pawn/, game.promote(nil, nil), "This is not a piece of chess.")
     assert_match(/black|white/, game.take_turn, "This is not black or white.")
-    assert_match(/black|white/, game.switch_player, "This is not black or white.")
+    assert_match(/black|white/, game.switch_players, "This is not black or white.")
     assert_match(/true|false/, game.checkmate?.to_s, "No pass test")
     assert_match(/true|false/, game.check?.to_s, "No pass test")
-    assert_match(/true|false/, board.move(nil, nil).to_s, "No pass test")
+    assert_match("[\"+   \", \"BP  \", \"+   \", \"+   \", \"+   \", \"+   \", \"+   \", \"+   \"]",
+    board.move("B1", "C1").to_s, "No pass test")
     assert_match(/true|false/, board.king_threatened?(nil, nil).to_s, "No pass test")
   end
 end

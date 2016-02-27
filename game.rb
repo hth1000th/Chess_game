544cb8eba12e432bc55c789a0d87a2810137c95a
Andy Hwang <tahwang@puc.edu>
2016-02-26 18:05:55 -0800

    new version of class Game

---
+++ game.rb
@@ -0,0 +1,60 @@
+require './board.rb'
+require './pieces.rb'
+
+class Game
+  attr_accessor :current_player, :current_piece
+  @@num = 0
+
+  def initialize()
+    @board = Board.new
+    @pieces = Pieces.new
+  end
+
+  def setup()
+    @board.draw_board # to create a chess board (8x8)
+    @pieces.draw_pieces # to place the pieces at the starting position
+  end
+
+  def remove(piece, from_location)
+  end
+
+  def add(piece, to_location)
+  end
+
+  def move()
+    remove(piece, from_location)
+    add(piece, to_location)
+  end
+
+  def promote(location, piece)
+    if (piece =~ /^(king|queen|bishop|knight|rook|pawn)$/i)
+      raise "Invalid input"
+    end
+    "bishop"
+  end
+
+  def take_turn()
+    "white"
+  end
+
+  def switch_players()
+    if(@@num%2 == 0)
+      @@num = @@num + 1
+      return "white"
+    else
+      @@num = @@num + 1
+      return "black"
+    end
+  end
+
+  def checkmate?()
+    true
+  end
+
+  def check?()
+    true
+  end
+
+  def valid_chess_move?(location1, location2)
+  end
+end

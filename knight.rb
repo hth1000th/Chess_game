5e52b66a0428ec0965fd7dd18ba86880715895c7
Andy Hwang <tahwang@puc.edu>
2016-03-04 15:49:12 -0800

    added a rule to move

--- knight.rb
+++ knight.rb
@@ -1,4 +1,12 @@
 class Knight
-  def rule_to_move(current_position, to_position)
+  def rule_to_move(from_x, from_y, to_x, to_y)
+    slope = (to_y - from_y) / (to_x - from_x).to_f
+    if slope == -0.5 || slope == 0.5
+      true
+    elsif slope == -2 || slope == 2
+      true
+    else
+      false
+    end
   end
 end

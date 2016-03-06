db66b901debd891fc82ac3c872614ff70a63f228
Andy Hwang <tahwang@puc.edu>
2016-03-06 13:58:36 -0800

    modified for a rule to move.

--- gamewindow.rb
+++ gamewindow.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require 'gosu'
 require 'rubygems'
 require './board.rb'
@@ -42,48 +41,3 @@ class GameWindow < Gosu::Window
     end
   end
 end
-=======
-require 'gosu'
-require 'rubygems'
-require './board.rb'
-require './pieces.rb'
-
-class GameWindow < Gosu::Window
-  def initialize
-    super 560,560,false
-    self.caption = "Chess Game"
-    @board = Board.new(self)
-    @pieces = Pieces.new(self)
-
-    @current_piece = ""
-    @click_to_move = 0
-    @hash = Hash.new
-  end
-
-  def draw
-    @board.draw_board
-    @pieces.draw_pieces
-  end
-
-  def button_down(id)
-    if (id == Gosu::MsLeft) && @click_to_move%2 == 0
-      @pieces.pieces.each { |key,value|
-        if value[0] == @pieces.mouse_to_board(mouse_x) && value[1] == @pieces.mouse_to_board(mouse_y)
-          @current_piece = key
-          break
-        end }
-      @click_to_move += 1
-    elsif (id == Gosu::MsLeft) && @click_to_move%2 != 0
-      @current_x = @pieces.mouse_to_board(mouse_x)
-      @current_y = @pieces.mouse_to_board(mouse_y)
-      if @pieces.valid_chess_move?(@current_piece, @pieces.pieces[@current_piece][0], @pieces.pieces[@current_piece][1], @current_x, @current_y)
-        @pieces.pieces[@current_piece] = [@current_x, @current_y]
-      else
-        p "Invalid move"
-      end
-      @click_to_move += 1
-      @current_piece = nil
-    end
-  end
-end
->>>>>>> f4373d99084344e764b26c9f4c79e8ee0bf3e0fb

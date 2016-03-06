407b31dff2b429e250a26249665cc150f8d16000
Andy Hwang <tahwang@puc.edu>
2016-03-06 14:01:07 -0800

    modified a rule to move

--- pawn.rb
+++ pawn.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Pawn
   def rule_to_move(from_x, from_y, to_x, to_y)
     if (to_x == from_x) && ((to_y - from_y) == -1)
@@ -10,16 +9,3 @@ class Pawn
     end
   end
 end
-=======
-class Pawn
-  def rule_to_move(from_x, from_y, to_x, to_y)
-    if (to_x == from_x) && ((to_y - from_y) == -1)
-      true
-    elsif (to_x == from_x) && ((to_y - from_y) == 1)
-      true
-    else
-      false
-    end
-  end
-end
->>>>>>> f4373d99084344e764b26c9f4c79e8ee0bf3e0fb

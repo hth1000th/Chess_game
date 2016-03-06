df81e5226fce1dac05a7f088f01e7def172525bf
Andy Hwang <tahwang@puc.edu>
2016-03-06 14:02:03 -0800

    modified a rule to move

--- queen.rb
+++ queen.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Queen
   def rule_to_move(from_x, from_y, to_x, to_y)
     if (to_x - from_x) == (to_y - from_y)
@@ -14,20 +13,3 @@ class Queen
     end
   end
 end
-=======
-class Queen
-  def rule_to_move(from_x, from_y, to_x, to_y)
-    if (to_x - from_x) == (to_y - from_y)
-      true
-    elsif (to_x - from_x) == (from_y - to_y)
-      true
-    elsif from_x == to_x
-      true
-    elsif from_y == to_y
-      true
-    else
-      false
-    end
-  end
-end
->>>>>>> f4373d99084344e764b26c9f4c79e8ee0bf3e0fb

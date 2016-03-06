941c25e841ed9b28b5ff6d14b5e2ca1f0cb3d531
Andy Hwang <tahwang@puc.edu>
2016-03-06 14:03:00 -0800

    START

--- run_game.rb
+++ run_game.rb
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 require './gamewindow.rb'
 
 window = GameWindow.new
 window.show
-=======
-require './gamewindow.rb'
-
-window = GameWindow.new
-window.show
->>>>>>> f4373d99084344e764b26c9f4c79e8ee0bf3e0fb

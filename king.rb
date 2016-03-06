e60dd39f522c4f827dce51e7a7637a543495b8fe
Andy Hwang <tahwang@puc.edu>
2016-03-06 14:00:33 -0800

    modified a rule to move

--- king.rb
+++ king.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class King
   def rule_to_move(from_x, from_y, to_x, to_y)
     if ((to_x - from_x) == 1 || (to_x - from_x) == -1) &&
@@ -17,23 +16,3 @@ class King
     end
   end
 end
-=======
-class King
-  def rule_to_move(from_x, from_y, to_x, to_y)
-    if ((to_x - from_x) == 1 || (to_x - from_x) == -1) &&
-      ((to_y - from_y) == 1 || (to_y - from_y) == -1)
-      true
-    elsif (to_x == from_x) && (to_y - from_y == 1)
-      true
-    elsif (to_x == from_x) && (to_y - from_y == -1)
-      true
-    elsif (to_y == from_y) && (to_x - from_x == 1)
-      true
-    elsif (to_y == from_y) && (to_x -from_x == -1)
-      true
-    else
-      false
-    end
-  end
-end
->>>>>>> f4373d99084344e764b26c9f4c79e8ee0bf3e0fb
